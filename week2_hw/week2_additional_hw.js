bc55bfb10f7a34b7230927c882a3f3e00a73f006
Aarik Robles <aarikrobles@Aariks-MacBook-Air.local>
2019-12-12 18:30:18 -0800

    Get Even answered

--- week2_hw/week2_additional_hw.js
+++ week2_hw/week2_additional_hw.js
@@ -7,7 +7,16 @@ CSS Basics 3
  */
 
 //  Part 2 
-// Write a for loop that will log the numbers 1 through 20
+// Easy Going, Write a for loop that will log the numbers 1 through 20
 for(let i = 1; i <= 20; i++){
     console.log(i)
+}
+
+// Get Even, Write a loop that will log only the even numbers in 0 through 200
+let i = 0
+while (i <= 200){
+    if (i % 2 === 0){
+        console.log(i)
+    }
+    i++
 }
