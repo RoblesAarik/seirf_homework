5b97a6ef40897a8d6d6a29c0e2298cbc9c587ee3
Aarik Robles <aarikrobles@Aariks-MacBook-Air.local>
2019-12-12 18:26:45 -0800

    Easy Going answered

---
+++ week2_hw/week2_additional_hw.js
@@ -0,0 +1,13 @@
+// Part 1
+/*
+HTML Intro 4
+HTML Elements 3
+CSS Intro 3
+CSS Basics 3
+ */
+
+//  Part 2 
+// Write a for loop that will log the numbers 1 through 20
+for(let i = 1; i <= 20; i++){
+    console.log(i)
+}
