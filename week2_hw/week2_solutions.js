4876d3f5965de03280cc7d49ee695dc145e6ad21
Aarik Robles <aarikrobles@Aariks-MacBook-Air.local>
2019-12-15 18:44:51 -0800

    sum array completed

--- week2_hw/week2_solutions.js
+++ week2_hw/week2_solutions.js
@@ -22,3 +22,12 @@ const checkPalindrome = (word) => {
 console.log(checkPalindrome("Radar"));
 console.log(checkPalindrome("Borscht"));
 
+// Sum Array
+const sumArray = (array) => {
+    let sum = 0
+    for(let i = 0; i < array.length; i++) {
+        sum += array[i]
+    } 
+    return sum
+}
+console.log(sumArray([1, 2, 3, 4, 5, 6]));
