d2382d07fba9e9fd144d1cb3d32742f25ee54416
Aarik Robles <aarikrobles@Aariks-MacBook-Air.local>
2019-12-23 12:14:25 -0800

    Week 3 hw

---
+++ week3_hw/answers.js
@@ -0,0 +1,47 @@
+/* Data type refreshers
+
+1. A light switch that can be either on or off.
+    datatype: boolean
+    ex: const light = true
+
+2. A user's email address.
+    datatype: string
+    ex: 'roblesaarik@gmail.com'
+
+3. A spaceship with a hull, laser blasters, tractor beam, and warp drive.
+    datatype: object
+    ex: const spaceship = {
+        area: 'hull',
+        weapon: 'laser blaster',
+        tool: 'tractor beam',
+        mobility: 'warp drive'
+    }
+
+A list of student names from our class.
+    datatype: array
+    ex: let names = ['Aarik','Loki','Alex'
+]
+A list of student names from our class, each with a location.
+    datatype: object
+    ex: const locations {
+        aarik: 'Pinole'
+        loki: 'New York'
+        alex: 'Chicago'
+    }
+A list of student names from our class, each with a location and each with a list of favorite tv shows.
+    datatype: array of objects
+    ex: [
+        aarik {
+            location: 'Pinole',
+            show: 'The Office',
+        },
+        loki {
+            location: 'New York',
+            show: 'Thor Ragnarock',
+        },
+        alex {
+            location: 'Chicago',
+            show: 'Breaking Bad'
+        }
+    ]
+*/
---
+++ week3_hw/landscaper/app.js
@@ -0,0 +1,36 @@
+console.log("It Works")
+/* We want to create a browser based text game 
+the goal is to make $1000
+one lawn per day
+cut a 
+start with teeth that make $1
+buy scissors at $5 and make $5 per day
+old timey push lawnmower cost $25, makes $50 per day
+powered lawnmover cost $250, make $100 per day
+poor workers at $500 per day, make $250 per day
+win at $1000
+
+The ability to take user input
+The ability for the user to earn money
+The ability to buy tools and use the new tool
+The ability to win the game and end it
+Hint: Try setting the win amount to $10 frst, then $50, and etc. until you get to the end to allow for easier testing!
+*/
+
+alert('Welcome to Lawnmower Tycoon')
+
+let balance = 0 
+
+
+let Teeth = 1; 
+let Scissors = 5;
+let OldLawnmower = 50;
+let BatteryLawnmower = 100;
+let PoorWorker = 250;
+
+const makeMoney = () => {
+    while (balance < 1000) {
+
+    }
+}
+
