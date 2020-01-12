765cd73d1da854c450b31a4311f71b6b790178db
Aarik Robles <aarikrobles@Aariks-MacBook-Air.local>
2020-01-11 16:46:52 -0800

    created timmys story

--- week4_hw/objects.js
+++ week4_hw/objects.js
@@ -1,10 +1,9 @@
 // Create Hamster class
 class Hamster {
-    constructor(name) {
+    constructor(name,owner) {
         this.name = name
+        this.owner = owner
     };
-
-    owner = "" ;
 
     price = 15;
 
@@ -21,16 +20,12 @@ class Hamster {
     }
 }
 
-me = new Hamster('Loki')
-console.log(me.wheelRun())
-
 // create person class
 class Person {
     constructor(name) {
         this.name = name
     };
-
-    age = 0; 
+    age = 0
 
     height = 0;
 
@@ -72,13 +67,65 @@ class Person {
         this.height += 1;
         this.weight += 1;
         this.mood -= 1;
-        this.bankAccount += 1;
+        this.bankAccount += 10;
     }
 
     buyHamster(hamster) {
-        hamster.push();
+       this.hamster.push(hamster);
         this.mood += 10
-        this.bankAccount -= Hamster.getPrice
+        this.bankAccount -= hamster.getPrice()
     }
 }
 
+// Create a story with person class
+// 1. Instantiate a new Person named Timmy
+const timmy = new Person ('Timmy')
+
+// 2. Age Timmy five years
+timmy.age = 5
+
+// 3. At this point Timmy's a little bummed. As a precocious child, he feels he's "seen it all" already. Have him eat five times.
+timmy.eat()
+timmy.eat()
+timmy.eat()
+timmy.eat()
+timmy.eat()
+
+
+// 4. Now Timmy's a little heavier than he wants to be. Kindergarten's coming up and he wants to look good. Have him exercise five times
+timmy.exercise()
+timmy.exercise()
+timmy.exercise()
+timmy.exercise()
+timmy.exercise()
+
+
+// 5. Age Timmy 9 years
+timmy.ageUp()
+timmy.ageUp()
+timmy.ageUp()
+timmy.ageUp()
+
+// 6. Create a hamster named "Gus"
+// 7. Set Gus's owner to the string "Timmy"
+gus = new Hamster('Gus','Timmy')
+console.log(gus)
+
+// 8. Have Timmy "buy" Gus
+timmy.buyHamster(gus)
+console.log(timmy)
+// 9. Age Timmy 15 years
+timmy.ageUp()
+timmy.ageUp()
+timmy.ageUp()
+timmy.ageUp()
+timmy.ageUp()
+timmy.ageUp()
+
+// 10. Have Timmy eat twice
+timmy.eat()
+timmy.eat()
+
+// 11.Have Timmy exercise twice
+timmy.exercise()
+timmy.exercise()
