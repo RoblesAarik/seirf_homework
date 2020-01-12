ccd4cf97aeaadcde7dcf42e6a508efd9688a0969
Aarik Robles <aarikrobles@Aariks-MacBook-Air.local>
2020-01-11 18:24:29 -0800

    Dougie and Pizza rat fought

--- week4_hw/donut_adventure/adventure.js
+++ week4_hw/donut_adventure/adventure.js
@@ -8,25 +8,41 @@ class Hero {
   
   health = 100;
 
-  weapons = {
-      sprinkleSpray: 5,
-      sugarShock: 10,
-  };
+  weapons = [
+      {
+        name: 'sprinkleSpray',
+        hitpoints: 5,
+    },
+      {
+         name: 'sugarShock',
+         hitpoints: 10,}
+  ]
 
   catchPhrases = ['I\'m fresher than day old pizza', 
   'you can\'t count my calories'];
+
+  randomNumGen (arr) {
+    return Math.floor(Math.random() * arr.length)
+}
+
+  randomWeapon() {
+    return this.weapons[this.randomNumGen(this.weapons)]
+ }
 
   talkSass() {
       console.log(this.catchPhrases[Math.floor(Math.random() * 2)])
   }; 
 
   announceHealth() {
-      console.log(this.health)
+      console.log(`My health is ${this.health}!`)
   };
 
-  fight() {
-      console.log(`I\'m ready to rumble`)
-  }
+  fight(enemy) {
+    let weapon = this.randomWeapon()
+    enemy.health -= weapon.hitpoints
+    console.log(`I\'m gonna flatten you like a slice of pepperoni!`)
+    return enemy.health
+}
 }
 
 dougie = new Hero('Dougie the donut')
@@ -39,27 +55,67 @@ class Enemy {
 
     health = 100;
 
-    weapons = {
-        pepperoniStars: 5,
-        cheeseGrease: 10    
-    };
+    weapons = [
+       { 
+           name: 'pepperoniStars',
+           hitpoints: 5,
+   
+        name: 'cheeseGrease',
+        hitpoints: 10 ,
+       }
+    ];   
+    
 
     catchPhrases = ['I\'m youtube famous',
     'I\'m more dangerous than an uncovered sewer'];
+
+    randomNumGen (arr) {
+        return Math.floor(Math.random() * arr.length)
+    }
+
+    randomWeapon() {
+       return this.weapons[this.randomNumGen(this.weapons)]
+    }
 
     talkSmack() {
         console.log(this.catchPhrases[Math.floor(Math.random() * 2)])
     };
 
     announceHealth() {
-        console.log(this.health)
+        console.log(`My health is ${this.health}!`)
     };
 
-    fight() {
+    fight(hero) {
+        let weapon = this.randomWeapon()
+        hero.health -= weapon.hitpoints
         console.log(`I\'m gonna flatten you like a slice of pepperoni!`)
+        return hero.health
     }
 
 }
 
 pizza = new Enemy('Pizza Rat')
+///////////////////////////////
+// Walking Down the Street
 
+// 1. Have Dougie talkSass
+console.log(dougie.talkSass())
+
+// 2. Have Pizza Rat talkSmack
+console.log(pizza.talkSmack())
+
+// 3. Have Dougie announceHealth
+console.log(dougie.announceHealth())
+
+// 4. Have Pizza Rat announceHealth
+console.log(pizza.announceHealth())
+
+// Fight
+
+// 1. Have Pizza Rat fight Dougie
+console.log(pizza.fight(dougie))
+// 2. Have Dougie fight Pizza Rat
+console.log(dougie.fight(pizza))
+// 3. Have Pizza Rat and Dougie both announceHealth to make sure their health has decreased!
+console.log(dougie.announceHealth())
+console.log(pizza.announceHealth())
