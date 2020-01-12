2884705fa9d1130bebda05abdc43523a0460e6f1
Aarik Robles <aarikrobles@Aariks-MacBook-Air.local>
2020-01-11 17:18:32 -0800

    created our enemy pizza rat

--- week4_hw/donut_adventure/adventure.js
+++ week4_hw/donut_adventure/adventure.js
@@ -1,5 +1,6 @@
 console.log('it works')
 
+// Created Hero class
 class Hero {
   constructor(name) {
       this.name = name
@@ -12,7 +13,7 @@ class Hero {
       sugarShock: 10,
   };
 
-  catchPhrases = ['i\'m fresher than day old pizza', 
+  catchPhrases = ['I\'m fresher than day old pizza', 
   'you can\'t count my calories'];
 
   talkSass() {
@@ -24,8 +25,41 @@ class Hero {
   };
 
   fight() {
-      console.log(`i\'m ready to rumble`)
+      console.log(`I\'m ready to rumble`)
   }
 }
 
+dougie = new Hero('Dougie the donut')
+
+// Created Enemy class
+class Enemy {
+    constructor(name) {
+        this.name = name
+    };
+
+    health = 100;
+
+    weapons = {
+        pepperoniStars: 5,
+        cheeseGrease: 10    
+    };
+
+    catchPhrases = ['I\'m youtube famous',
+    'I\'m more dangerous than an uncovered sewer'];
+
+    talkSmack() {
+        console.log(this.catchPhrases[Math.floor(Math.random() * 2)])
+    };
+
+    announceHealth() {
+        console.log(this.health)
+    };
+
+    fight() {
+        console.log(`I\'m gonna flatten you like a slice of pepperoni!`)
+    }
+
+}
+
+pizza = new Enemy('Pizza Rat')
 
