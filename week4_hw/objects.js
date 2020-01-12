0889ea4d9b4b3d0531754f0f4e677455a5761b84
Aarik Robles <aarikrobles@Aariks-MacBook-Air.local>
2020-01-11 16:13:24 -0800

    created person class

--- week4_hw/objects.js
+++ week4_hw/objects.js
@@ -23,3 +23,62 @@ class Hamster {
 
 me = new Hamster('Loki')
 console.log(me.wheelRun())
+
+// create person class
+class Person {
+    constructor(name) {
+        this.name = name
+    };
+
+    age = 0; 
+
+    height = 0;
+
+    weight = 0;
+
+    mood = 0;
+
+    hamster = [];
+
+    bankAccount = 0;
+
+    getName() {
+        return this.name
+    };
+
+    getAge() {
+        return this.age
+    };
+
+    getWeight() {
+        return this.weight
+    };
+
+    greet() {
+        console.log(`Hi ${this.name}`)
+    };
+
+    eat() {
+        this.weight += 1;
+        this.mood += 1
+    };
+
+    exercise() {
+        this.weight -= 1
+    };
+
+    ageUp() {
+        this.age += 1;
+        this.height += 1;
+        this.weight += 1;
+        this.mood -= 1;
+        this.bankAccount += 1;
+    }
+
+    buyHamster(hamster) {
+        hamster.push();
+        this.mood += 10
+        this.bankAccount -= Hamster.getPrice
+    }
+}
+
