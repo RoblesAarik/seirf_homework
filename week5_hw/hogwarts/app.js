012e13dce5cbcb211356424fcc91bdbd4451990f
Aarik Robles <aarikrobles@Aariks-MacBook-Air.local>
2020-01-20 00:03:08 -0600

    Commit 3 - I added list elements.

--- week5_hw/hogwarts/app.js
+++ week5_hw/hogwarts/app.js
@@ -1,11 +1,18 @@
 // Hogwarts hw
 // jQuery functions
 $(() => {
-  hogwarts();
-  name();
-  house();
-  pet();
-  wand();
+  $hogwarts();
+  $name();
+  $house();
+  $pet();
+  $wand();
+  $trunk();
+  $items("Butter Beer");
+  $secret("Invisibility Cloak");
+  $secret("Magic Map");
+  $secret("Time Turner");
+  $leash();
+  $items("Bertie Bott's Every Flavor [Jelly] Beans");
 });
 
 // create a div with an id of container
@@ -13,33 +20,61 @@ const $container = $("#container");
 console.log($container);
 
 // Create an h1 element
-const hogwarts = () => {
+const $hogwarts = () => {
   let $h1 = $("<h1>");
   $h1.text("Hogwarts");
   $($h1).appendTo("#container");
 };
 
 // Create h2 with your name
-const name = () => {
+const $name = () => {
   let $h2 = $("<h2>").text("Aarik Robles");
   $($h2).appendTo("#container");
 };
 
 // Create h3 with your house
-const house = () => {
+const $house = () => {
   let $h3 = $("<h3>").text("Hufflepuff");
   $($h3).appendTo("#container");
 };
 
 // Create h4 with your pet
-const pet = () => {
+const $pet = () => {
   let $h4 = $("<h4>").text("Loki");
   $h4.addClass("owl");
   $($h4).appendTo("#container");
 };
 
 // Create h4 with your wand
-const wand = () => {
+const $wand = () => {
   let $h4 = $("<h4>").text("Laurel wood with a unicorn hair core");
   $($h4).appendTo("#container");
+};
+
+// Create ul with attr of storage and value of trunk
+const $trunk = () => {
+  let $ul = $("<ul>").attr("storage", "trunk");
+  $($ul).appendTo("#container");
+};
+
+// Create list items
+const $items = item => {
+  let $li = $("<li>").text(item);
+  $($li).appendTo("ul");
+};
+
+// Create items with secret class
+const $secret = item => {
+  let $li = $("<li>")
+    .addClass("secret")
+    .text(item);
+  $($li).appendTo("ul");
+};
+
+// Create leash with class same as pet
+const $leash = () => {
+  let $li = $("<li>")
+    .text("leash")
+    .addClass("owl");
+  $($li).appendTo("ul");
 };
