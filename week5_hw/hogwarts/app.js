3ff9aa1453f4c244f4d3b00b21ea1d026285bca8
Aarik Robles <aarikrobles@Aariks-MacBook-Air.local>
2020-01-19 23:26:34 -0600

    Commit 2 - I added more element. My power grows with my dedication and hard work

--- week5_hw/hogwarts/app.js
+++ week5_hw/hogwarts/app.js
@@ -1,14 +1,45 @@
 // Hogwarts hw
+// jQuery functions
+$(() => {
+  hogwarts();
+  name();
+  house();
+  pet();
+  wand();
+});
 
 // create a div with an id of container
 const $container = $("#container");
 console.log($container);
+
 // Create an h1 element
-const createH1 = () => {
+const hogwarts = () => {
   let $h1 = $("<h1>");
   $h1.text("Hogwarts");
   $($h1).appendTo("#container");
 };
-$(() => {
-  createH1();
-});
+
+// Create h2 with your name
+const name = () => {
+  let $h2 = $("<h2>").text("Aarik Robles");
+  $($h2).appendTo("#container");
+};
+
+// Create h3 with your house
+const house = () => {
+  let $h3 = $("<h3>").text("Hufflepuff");
+  $($h3).appendTo("#container");
+};
+
+// Create h4 with your pet
+const pet = () => {
+  let $h4 = $("<h4>").text("Loki");
+  $h4.addClass("owl");
+  $($h4).appendTo("#container");
+};
+
+// Create h4 with your wand
+const wand = () => {
+  let $h4 = $("<h4>").text("Laurel wood with a unicorn hair core");
+  $($h4).appendTo("#container");
+};
