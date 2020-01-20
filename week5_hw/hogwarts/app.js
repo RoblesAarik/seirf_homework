4de6f23260c7c456d66526de8582b1e0fe9dc9d4
Aarik Robles <aarikrobles@Aariks-MacBook-Air.local>
2020-01-19 23:05:51 -0600

    Commit 1 - I added my first element and it was fun

--- week5_hw/hogwarts/app.js
+++ week5_hw/hogwarts/app.js
@@ -1,1 +1,14 @@
+// Hogwarts hw
 
+// create a div with an id of container
+const $container = $("#container");
+console.log($container);
+// Create an h1 element
+const createH1 = () => {
+  let $h1 = $("<h1>");
+  $h1.text("Hogwarts");
+  $($h1).appendTo("#container");
+};
+$(() => {
+  createH1();
+});
