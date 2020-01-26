edc7e0fd56a9cf639052ed8e7f0a77f97d2eb28a
Aarik Robles <roblesaarik@gmail.com>
2020-01-26 10:50:39 -0800

    Part one

--- week6_hw/todos_starter/app.js
+++ week6_hw/todos_starter/app.js
@@ -1,3 +1,15 @@
 $(() => {
-  console.log("Works");
+  const list = [];
+
+  const handleCLick = () => {
+    const inputBoxValue = $("#input-box").val();
+
+    console.log(inputBoxValue);
+
+    list.push(inputBoxValue);
+
+    $(event.target).trigger("reset");
+  };
+
+  $("#submit").on("click", handleCLick);
 });
