ccca667c144857556b1fb136c3ab2f58fc21799a
Aarik Robles <roblesaarik@gmail.com>
2020-01-26 11:43:37 -0800

    part three

--- week6_hw/todos_starter/app.js
+++ week6_hw/todos_starter/app.js
@@ -1,6 +1,25 @@
 $(() => {
   const list = [];
 
+  // Create the ul under both Things to do and completed
+  const createList = (listId, ulClass) => {
+    let $ul = $("<ul>").addClass(ulClass);
+    $(listId).append($ul);
+  };
+
+  createList("#to-do-list", "todo");
+  createList("#completed", "done");
+
+  // Create the items that will go into the list
+  const createListItems = () => {
+    let $li = $("<li>");
+    $(".todo").empty();
+    list.forEach(item => {
+      $(".todo").append("<li>" + item + "</li>");
+    });
+  };
+
+  // Action that will
   const handleCLick = () => {
     const inputBoxValue = $("#input-box").val();
 
@@ -8,7 +27,11 @@ $(() => {
 
     list.push(inputBoxValue);
 
-    $(event.target).trigger("reset");
+    event.preventDefault();
+
+    $(event.currentTarget).trigger("reset");
+
+    createListItems();
   };
 
   $("#submit").on("click", handleCLick);
