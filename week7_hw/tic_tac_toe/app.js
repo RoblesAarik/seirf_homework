3284d2b976afc822cf027a5b447b6770d18d125a
Aarik Robles <roblesaarik@gmail.com>
2020-01-26 17:29:15 -0800

    squares makes X and O

--- week7_hw/tic_tac_toe/app.js
+++ week7_hw/tic_tac_toe/app.js
@@ -25,11 +25,32 @@ $(() => {
 
   const handleClick = event => {
     let $currentShape = $(event.currentTarget);
-    $currentShape.text(`${shapes[i]}`);
+    $currentShape.text(shapes[0]);
     $currentShape.removeClass();
     $currentShape.addClass(shapes[0]);
     shapes.push(shapes[0]);
     shapes.shift([0]);
+  };
+
+  // win conditions
+  const checkWin = () => {
+    // if top row has 3 X's or 3 O's
+    if (
+      $("#1").text() === "X" &&
+      $("#2").text() === "X" &&
+      $("#3").text() === "X"
+    ) {
+      alert("X wins");
+    } else {
+      false;
+    }
+    // if middle row has 3 X's or 3 O's
+    // if bottom row has 3 X's or 3 O's
+    // if left column has 3 X's or 3 O's
+    // if middle column has 3 X's or 3 O's
+    // if right column has 3 X's or 3 O's
+    // if left diagnal has 3 X's or 3 O's
+    // if right diagnal has 3 X's or 3 O's
   };
 
   $(".square").on("click", handleClick);
