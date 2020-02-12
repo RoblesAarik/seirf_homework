57e897b0d572c35519e406e2012efce5463d43fe
Aarik Robles <roblesaarik@gmail.com>
2020-02-12 12:40:07 -0800

    Magic 8 Ball express application created

--- week9_hw/express_hw/server.js
+++ week9_hw/express_hw/server.js
@@ -12,6 +12,37 @@ app.get("/tip/:total/:tipPercentage", (req, res) => {
   res.send("TIP amount is $" + amount);
 });
 
+app.get("/magic/:question", (req, res) => {
+  let response = [
+    "It is certain",
+    "It is decidedly so",
+    "Without a doubt",
+    "Yes definitely",
+    "You may rely on it",
+    "As I see it yes",
+    "Most likely",
+    "Outlook good",
+    "Yes",
+    "Signs point to yes",
+    "Reply hazy try again",
+    "Ask again later",
+    "Better not tell you now",
+    "Cannot predict now",
+    "Concentrate and ask again",
+    "Don't count on it",
+    "My reply is no",
+    "My sources say no",
+    "Outlook not so good",
+    "Very doubtful",
+  ];
+  let answer = response[Math.floor(Math.random() * response.length)];
+  res.send(`<html>
+  <body>
+  <h1>${answer}<h1>
+  <body> 
+  <html>`);
+});
+
 app.listen(port, () => {
   console.log("listening on port", port);
 });
