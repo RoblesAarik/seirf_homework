9d2ff967db22450539216352b868673d457e236a
Aarik Robles <roblesaarik@gmail.com>
2020-02-12 10:02:35 -0800

    Tip Calc express application created

--- week9_hw/express_hw/server.js
+++ week9_hw/express_hw/server.js
@@ -6,6 +6,12 @@ app.get("/greeting/:name", (req, res) => {
   res.send(`Wow! Hello there, ${req.params.name}`);
 });
 
+app.get("/tip/:total/:tipPercentage", (req, res) => {
+  let percentage = req.params.tipPercentage / 100;
+  let amount = req.params.total * percentage;
+  res.send("TIP amount is $" + amount);
+});
+
 app.listen(port, () => {
   console.log("listening on port", port);
 });
