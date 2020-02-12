66cba6f4ecfe0f75f7eaed3cf2429215abe4312e
Aarik Robles <roblesaarik@gmail.com>
2020-02-12 09:54:50 -0800

    Greeting express application created.

--- week9_hw/express_hw/server.js
+++ week9_hw/express_hw/server.js
@@ -2,6 +2,10 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+app.get("/greeting/:name", (req, res) => {
+  res.send(`Wow! Hello there, ${req.params.name}`);
+});
+
 app.listen(port, () => {
   console.log("listening on port", port);
 });
