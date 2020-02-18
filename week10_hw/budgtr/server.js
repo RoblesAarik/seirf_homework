dcbd6a44e761d8c356cf2df7e4b78297c794e5ca
Aarik Robles <roblesaarik@gmail.com>
2020-02-18 14:56:31 -0800

    finished setup

--- week10_hw/budgtr/server.js
+++ week10_hw/budgtr/server.js
@@ -2,6 +2,11 @@ const express = require("express");
 const app = express;
 const port = 3000;
 
+app.use(express.static("public"));
+
+app.get("/budget", (req, res) => {
+  res.send("it works");
+});
 app.listen(port, () => {
   console.log("listening at port 3000");
 });
