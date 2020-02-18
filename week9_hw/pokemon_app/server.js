23a9503040bafe116a7dd79d2c8f85a53fd84703
Aarik Robles <roblesaarik@gmail.com>
2020-02-18 13:55:42 -0800

    Created show views of each pokemon

--- week9_hw/pokemon_app/server.js
+++ week9_hw/pokemon_app/server.js
@@ -15,7 +15,9 @@ app.get("/pokemon", (req, res) => {
 });
 
 app.get("/pokemon/:id", (req, res) => {
-  res.send(req.params.id);
+  res.render("show.ejs", {
+    pokemon: pokemon[req.params.id],
+  });
 });
 
 app.listen(port, () => {
