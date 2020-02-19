938c7ad1fb0943af1610a59253aa69d767b28e6e
Aarik Robles <roblesaarik@gmail.com>
2020-02-19 14:56:59 -0800

    Finished new route

--- week10_hw/budgtr/server.js
+++ week10_hw/budgtr/server.js
@@ -20,11 +20,30 @@ app.get("/budget", (req, res) => {
   });
 });
 
+// New Page
+app.get("/budget/new", (req, res) => {
+  res.render("new.ejs");
+});
+
 // Show Page
 app.get("/budget/:id", (req, res) => {
   res.render("show.ejs", {
     budget: budget[req.params.id],
   });
+});
+
+// Post route that creates new item
+app.post("/budget", (req, res) => {
+  console.log(req.body);
+  let item = {};
+  item.date = req.body.date;
+  item.name = req.body.name;
+  item.from = req.body.from;
+  item.amount = req.body.amount;
+  item.tags = req.body.tags;
+  budget.push(item);
+  console.log("budget", budget);
+  res.redirect("/budget");
 });
 
 app.listen(port, () => {
