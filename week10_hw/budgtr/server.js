0a7f305eaa16c8da8e815cd485c006859c2823ca
Aarik Robles <roblesaarik@gmail.com>
2020-02-19 13:51:19 -0800

    completed show page

--- week10_hw/budgtr/server.js
+++ week10_hw/budgtr/server.js
@@ -1,14 +1,29 @@
+// Setup
 const express = require("express");
 const app = express();
 const port = 3000;
 
 const budget = require("./models/budget.js");
 
-app.use(express.static("public"));
+// Middleware
+app.use((req, res, next) => {
+  console.log("I run for all routes");
+  next();
+});
 
+app.use(express.urlencoded({ extended: false }));
+
+// Index Page
 app.get("/budget", (req, res) => {
   res.render("index.ejs", {
     budget: budget,
+  });
+});
+
+// Show Page
+app.get("/budget/:id", (req, res) => {
+  res.render("show.ejs", {
+    budget: budget[req.params.id],
   });
 });
 
