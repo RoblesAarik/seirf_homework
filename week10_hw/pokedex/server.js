9dce900bf72e0d6ef780ae9a6de5d5b3517deb56
Aarik Robles <roblesaarik@gmail.com>
2020-02-21 13:08:59 -0800

    added imgs to index page

--- week10_hw/pokedex/server.js
+++ week10_hw/pokedex/server.js
@@ -1,11 +1,17 @@
 // Setup
 const express = require("express");
-const app = express;
+
+const app = express();
+
 const port = 3000;
+
+const pokemon = require("./models/pokemon.js");
 
 // Index page
 app.get("/pokemon", (req, res) => {
-  res.send("Works");
+  res.render("index.ejs", {
+    pokemon: pokemon,
+  });
 });
 
 app.listen(port, () => {
