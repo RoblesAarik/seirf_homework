41f9ff6e799bb376337e5e2d01a8859213eeb2f2
Aarik Robles <roblesaarik@gmail.com>
2020-02-21 17:09:48 -0800

    finished show page

--- week10_hw/pokedex/server.js
+++ week10_hw/pokedex/server.js
@@ -14,6 +14,13 @@ app.get("/pokemon", (req, res) => {
   });
 });
 
+// Show Page
+app.get("/pokemon/:id", (req, res) => {
+  res.render("show.ejs", {
+    pokemon: pokemon[req.params.id],
+  });
+});
+
 app.listen(port, () => {
   console.log("listening");
 });
