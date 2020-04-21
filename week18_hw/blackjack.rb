874a287d89893a002ac66ebe96deb5645ce5008b
Aarik Robles <roblesaarik@gmail.com>
2020-04-20 17:43:45 -0700

    created game logic

--- week18_hw/blackjack.rb
+++ week18_hw/blackjack.rb
@@ -3,49 +3,86 @@
 # Player Classes
 
 class Player
-    attr_accessor :name
-    attr_accessor :bankroll
-    attr_accessor :hand
+    attr_accessor :name, :bankroll, :hand, :sum
     def initialize name
         @name = name
         @bankroll = 100
         @hand = []
+        @sum = sum
     end 
 
-    def name
-        @name
+    def name=value
+        @name=value
+    end
+
+    def player_hand deck
+        self.hand.push(deck[0])
+        deck.shift
+    end
+
+
+
+    def sum
+        if self.hand.length == 0
+            sum = 0
+        else
+        @sum = self.hand[0].value + self.hand[1].value
+        end
     end
 
 end
 
+
+
 class Dealer
     attr_reader :name
     attr_accessor :bankroll, :hand
-    def initialize name
+    def initialize
         @name = 'Dealer'
-        @bankroll = 1000
+        @bankroll = 10000
         @hand = []
+        @sum = sum
     end 
 
+    def dealer_hand deck
+        self.hand.push(deck[0])
+        deck.shift
+    end
+
+    def sum
+        if self.hand.length == 0
+            @sum = 0
+        else
+        @sum = self.hand[0].value + self.hand[1].value
+        end
+    end
+
 
 end
 
 
-# Empty deck
 
-def deck
-    deck = []
+# create new player
+def create_players
+    name = gets.chomp
+    $player1 = Player.new name
+    $dealer1 = Dealer.new
+    # p $player1
+    # p $dealer1
 end
+
+
 
 # Cards
 
 class Cards
-    attr_reader :face,:value
+    attr_reader :face,:value, :suits
     def initialize face, value, suits
         @face = face
         @value = value
         @suits = suits
     end
+
 end
 
 # Create the deck of cards. 
@@ -58,14 +95,70 @@ def create_deck deck
         
         values.each{ |number|
             card = Cards.new number[0], number[1], suit
-            p card
             deck.push(card)
         }
         
     }
 
-    p deck
 end
 
-create_deck(deck)
+ 
 
+# Shuffle deck
+def shuffle deck
+    deck.shuffle!
+end
+
+
+# get deck and shuffle cards
+def ready_cards deck
+create_deck deck
+shuffle deck
+# p deck.length
+end
+
+
+# Deal cards
+def deal_cards deck
+    $player1.player_hand deck
+    $dealer1.dealer_hand deck
+    $player1.player_hand deck
+    $dealer1.dealer_hand deck
+    p "Your cards: #{$player1.hand[0].face} of #{$player1.hand[0].suits} and #{$player1.hand[1].face} of #{$player1.hand[1].suits}"
+    p "Dealers has a #{$dealer1.hand[1].face} of #{$dealer1.hand[1].suits}"
+end
+
+
+
+
+# Game Logic
+
+def check_win
+    if $player1.sum > $dealer1.sum
+        p "You win!"
+    elsif $player.sum < $dealer1.sum
+        p "Dealer wins"
+    elsif $player1.sum == $dealer1.sum
+        p "Tie try again"
+    end
+    p "Your score: #{$player1.sum}"
+    p "Dealer Score: #{$dealer1.sum}"
+end
+
+def start_game
+    create_players
+    p "Welcome #{$player1.name}"
+end
+
+def round1
+    deck =[]
+    ready_cards deck
+    deal_cards deck
+    check_win
+   
+end
+
+
+
+start_game
+round1
