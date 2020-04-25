25b94a08fe33856cad11ffda324f667be478f9e6
Aarik Robles <roblesaarik@gmail.com>
2020-04-25 14:02:12 -0700

    Commit 11: Relations

--- week19_hw/matrix_api_app/db/seeds.rb
+++ week19_hw/matrix_api_app/db/seeds.rb
@@ -7,7 +7,11 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 Character.create([
 	{ name: "Neo", description: "Neo (born as Thomas A. Anderson, also known as The One, an anagram for Neo) is a fictional character and the protagonist of the Matrix franchise." },
-	{ name: "Trinity", description: "Trinity is a fictional character in the Matrix franchise." },
-	{ name: "Morpheus", description: "Morpheus is a fictional character in The Matrix franchise." },
-	{ name: "Agent Smith", description: "Agent Smith is a fictional character and the main antagonist of The Matrix trilogy." },
+	{ name: "Trinity", description: "Trinity is a fictional character in the Matrix franchise.", vehicle_id: 1 },
+	{ name: "Morpheus", description: "Morpheus is a fictional character in The Matrix franchise.", vehicle_id: 1 },
+	{ name: "Agent Smith", description: "Agent Smith is a fictional character and the main antagonist of The Matrix trilogy.",  },
+])
+
+Vehicles.create([
+    {name: "Nebuchadnezzar", style: "Spaceship"}
 ])
