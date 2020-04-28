0291f4dfa542d30a0d07ac32e8fe50c95917de03
Aarik Robles <roblesaarik@gmail.com>
2020-04-27 19:39:37 -0700

    Added routes and controller

---
+++ week20_hw/tweeter_app_api/app/controllers/tweets_controller.rb
@@ -0,0 +1,15 @@
+class TweetsController < ApplicationController
+
+    def index
+        tweet = Tweet.all
+        render json: {status: 200, tweet: tweet}
+    end
+
+    def show 
+        found_tweet = Tweet.find(params[:id])
+        render json: {status: 200, tweet: found_tweet}
+    end
+
+
+
+end
