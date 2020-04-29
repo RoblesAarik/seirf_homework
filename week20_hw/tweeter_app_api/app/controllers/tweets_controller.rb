4bdfe534dce3f26149a0969890bd32415f02e5e5
Aarik Robles <roblesaarik@gmail.com>
2020-04-28 21:51:53 -0700

    finished

--- week20_hw/tweeter_app_api/app/controllers/tweets_controller.rb
+++ week20_hw/tweeter_app_api/app/controllers/tweets_controller.rb
@@ -10,6 +10,32 @@ class TweetsController < ApplicationController
         render json: {status: 200, tweet: found_tweet}
     end
 
+    def create
+        tweet = Tweet.new(tweet_params)
+
+        if tweet.save
+            render(status: 201, json: {tweet: tweet})
+        else
+            render(status: 422, json: {tweet: tweet})
+        end
+    end
+
+    def update
+        tweet = Tweet.find(params[:id])
+        tweet.update(tweet_params)
+        render(json: {tweet: tweet})
+    end
+
+    def destroy
+        tweet = Tweet.destroy(params[:id])
+        render(status:204)
+    end
+
+
+    private
+    def tweet_params
+        params.require(:tweet).permit(:author, :title, :content)
+    end
 
 
 end
