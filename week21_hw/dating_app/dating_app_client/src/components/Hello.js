005c751eb032f9c89f78416eb0283de87422eb4f
Aarik Robles <roblesaarik@gmail.com>
2020-05-06 17:57:02 -0700

    Made React App

---
+++ week21_hw/dating_app/dating_app_client/src/components/Hello.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Daters extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello Dating World</h1>
+      </div>
+    );
+  }
+}
+
+export default Daters;
